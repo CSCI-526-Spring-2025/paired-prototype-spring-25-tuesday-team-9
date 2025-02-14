fb3e24fe7babd5dd536a5c3ccc1afbbd114a75ee
Jenny Wang <65201542+aurorayihe@users.noreply.github.com>
2025-02-13 23:56:28 -0800

    human mode level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject humanForm;  // Reference to the human GameObject
+    public GameObject ballForm;   // Reference to the ball GameObject
+    public Vector3 offset = new Vector3(0, 2, -10); // Camera offset from player
+    public float smoothSpeed = 5f;  // Speed of camera smoothing
+
+    private Transform currentTarget; // Tracks the currently active player form
+
+    void LateUpdate()
     {
-        
+        UpdateTarget();
+
+        if (currentTarget != null)
+        {
+            // Calculate the target position
+            Vector3 targetPosition = currentTarget.position + offset;
+
+            // Smoothly move the camera towards the target
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateTarget()
     {
-        
+        // Determine which form is active
+        if (humanForm.activeSelf)
+        {
+            currentTarget = humanForm.transform;
+        }
+        else if (ballForm.activeSelf)
+        {
+            currentTarget = ballForm.transform;
+        }
     }
 }
